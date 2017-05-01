89746d6702cc68c9bf177cd56ebfa9f7821ce7db
PWAckerman <patrickackerman@gmail.com>
2017-05-01 16:58:04 -0400

    Changes to Dockerfile configuration; Server side react routing

--- server/server.js
+++ server/server.js
@@ -5,32 +5,37 @@ const app = express();
 app.use(express.static('../dist'));
 
 //quick and dirty react server-side routing
+app.get('/', (res, res)=>{
+    console.log(res);
+    res.send(fs.readFileSync('../dist/index.html','utf8'));
+})
+
 app.get('/home',(req,res)=>{
-    res.send(fs.readFileSync('../dist/index.html','utf8'))
+    res.send(fs.readFileSync('../dist/index.html','utf8'));
 })
 
 app.get('/contact',(req,res)=>{
-    res.send(fs.readFileSync('../dist/index.html','utf8'))
+    res.send(fs.readFileSync('../dist/index.html','utf8'));
 })
 
 app.get('/contact/email',(req,res)=>{
-    res.send(fs.readFileSync('../dist/index.html','utf8'))
+    res.send(fs.readFileSync('../dist/index.html','utf8'));
 })
 
 app.get('/contact/text',(req,res)=>{
-    res.send(fs.readFileSync('../dist/index.html','utf8'))
+    res.send(fs.readFileSync('../dist/index.html','utf8'));
 })
 
 app.get('/microblog',(req,res)=>{
-    res.send(fs.readFileSync('../dist/index.html','utf8'))
+    res.send(fs.readFileSync('../dist/index.html','utf8'));
 })
 
 app.get('/education',(req,res)=>{
-    res.send(fs.readFileSync('../dist/index.html','utf8'))
+    res.send(fs.readFileSync('../dist/index.html','utf8'));
 })
 
 app.get('/projects',(req,res)=>{
-    res.send(fs.readFileSync('../dist/index.html','utf8'))
+    res.send(fs.readFileSync('../dist/index.html','utf8'));
 })
 
 app.listen(5000, ()=>{
