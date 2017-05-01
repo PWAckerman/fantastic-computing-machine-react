6c1e3dc3c4f4b5b5fd3bace7c7cd89d849691ff8
PWAckerman <patrickackerman@gmail.com>
2017-05-01 17:45:40 -0400

    changes to server.js for routing and file system pathing

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const app = express();
 
 app.use(express.static('../dist'));
@@ -7,35 +8,35 @@ app.use(express.static('../dist'));
 //quick and dirty react server-side routing
 app.get('/', (req, res)=>{
     console.log(res);
-    res.send(fs.readFileSync('../dist/index.html','utf8'));
+    res.send(fs.readFileSync(path.join(__dirname, '../dist/index.html'),'utf8'));
 })
 
 app.get('/home',(req,res)=>{
-    res.send(fs.readFileSync('../dist/index.html','utf8'));
+    res.send(fs.readFileSync(path.join(__dirname, '../dist/index.html'),'utf8'));
 })
 
 app.get('/contact',(req,res)=>{
-    res.send(fs.readFileSync('../dist/index.html','utf8'));
+    res.send(fs.readFileSync(path.join(__dirname, '../dist/index.html'),'utf8'));
 })
 
 app.get('/contact/email',(req,res)=>{
-    res.send(fs.readFileSync('../dist/index.html','utf8'));
+    res.send(fs.readFileSync(path.join(__dirname, '../dist/index.html'),'utf8'));
 })
 
 app.get('/contact/text',(req,res)=>{
-    res.send(fs.readFileSync('../dist/index.html','utf8'));
+    res.send(fs.readFileSync(path.join(__dirname, '../dist/index.html'),'utf8'));
 })
 
 app.get('/microblog',(req,res)=>{
-    res.send(fs.readFileSync('../dist/index.html','utf8'));
+    res.send(fs.readFileSync(path.join(__dirname, '../dist/index.html'),'utf8'));
 })
 
 app.get('/education',(req,res)=>{
-    res.send(fs.readFileSync('../dist/index.html','utf8'));
+    res.send(fs.readFileSync(path.join(__dirname, '../dist/index.html'),'utf8'));
 })
 
 app.get('/projects',(req,res)=>{
-    res.send(fs.readFileSync('../dist/index.html','utf8'));
+    res.send(fs.readFileSync(path.join(__dirname, '../dist/index.html'),'utf8'));
 })
 
 app.listen(5000, ()=>{
