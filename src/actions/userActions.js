afbfc7cbb8f843230a2e224d114adb0fcd1a7533
PWAckerman <patrickackerman@gmail.com>
2017-05-03 18:17:08 -0400

    user actions

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -8,6 +8,14 @@ export function getUserSuccess(user){
     return {type: types.GET_USER_SUCCESS, user}
 }
 
+export function sendEmailSuccess(emailData){
+    return {type: types.SEND_EMAIL_SUCCESS, emailData }
+}
+
+export function sendTextSuccess(textData){
+    return {type: types.SEND_TEXT_SUCCESS, textData }
+}
+
 export function loadUser(dispatch, id){
     return function(dispatch){
         userService.getUser(id).then((user)=>{
@@ -15,5 +23,37 @@ export function loadUser(dispatch, id){
         }).catch(error => {
             throw(error);
         });
+    }
+}
+
+export function sendEmail(dispatch, data){
+    console.log('email', dispatch);
+    return function(dispatch){
+        let body = {
+            from: data.from,
+            subject: data.subject,
+            text: data.message
+        }
+        userService.sendEmail(body).then((data)=>{
+            dispatch(sendEmailSuccess(data));
+        }).catch(err => {
+            throw(err);
+        })
+    }
+}
+
+export function sendText(dispatch, data){
+    return function(dispatch){
+        let body = {
+            sender: data.from,
+            company: data.message,
+            number: 'number',
+            email: "example@exemplar.com"
+        }
+        userService.sendText(body).then((data)=>{
+            dispatch(sendTextSuccess(data));
+        }).catch(err => {
+            throw(err);
+        })
     }
 }
